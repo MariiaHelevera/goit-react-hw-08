e05937486412383628875338cf181285713a9fee
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-06-15 23:01:03 +0200

    quick save

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://66671269a2f8516ff7a6304a.mockapi.io/';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
